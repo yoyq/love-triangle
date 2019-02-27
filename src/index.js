c34410bebbdad4cd133e21445885e6a478b17d13
dima <3277898@gmail.com>
2019-02-27 21:04:04 +0300

    love-triangle solve task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    let checkMas = [];
+
+    for (let i = 0; i < preferences.length; i++) {
+        if (!checkMas.length) {
+            checkMas.push(i);
+        }
+        while (checkMas.length !== 4) {
+            let nextSpichonee = preferences[checkMas[checkMas.length-1]];
+            checkMas.push(nextSpichonee-1);
+        }
+
+        if (checkMas[0] === checkMas[3] && checkMas[1] !== checkMas[2]) {
+            count++;
+
+            checkMas.forEach((v, i, a) => {
+                delete preferences[v];
+            });
+        }
+
+        checkMas.length = 0;
+    }
+
+    return count;
 };
